7abcf79b7846b94dc1345f5917edb7a466aea987
adeladelman0 <adelhesham20001@gmail.com>
2026-09-19 05:06:21 -0700

    feat: wire intelligence AI service

--- libraries/nestjs-libraries/src/social-intelligence/social-intelligence.module.ts
+++ libraries/nestjs-libraries/src/social-intelligence/social-intelligence.module.ts
@@ -2,13 +2,19 @@ import { Module } from '@nestjs/common';
 import { SocialIntelligenceService } from './social-intelligence.service';
 import { SocialIntelligenceRepository } from './social-intelligence.repository';
 import { SocialIntelligenceDatabase } from './social-intelligence.database';
+import { SocialIntelligenceAiService } from './social-intelligence.ai.service';
 
 @Module({
   providers: [
     SocialIntelligenceDatabase,
     SocialIntelligenceService,
     SocialIntelligenceRepository,
+    SocialIntelligenceAiService,
   ],
-  exports: [SocialIntelligenceService, SocialIntelligenceRepository],
+  exports: [
+    SocialIntelligenceService,
+    SocialIntelligenceRepository,
+    SocialIntelligenceAiService,
+  ],
 })
 export class SocialIntelligenceModule {}
